f9c82a73c9a6c0d285963ae3bfec3a0055c9cba8
Roger Marques <marquesroger226@gmail.com>
2023-04-07 18:03:47 -0300

    [feat]: Adicionando novos projetos

--- src/data/projects.js
+++ src/data/projects.js
@@ -18,6 +18,24 @@ const projects = [
     nascer e pôr do sol.`,
     link: 'https://weather-app-eta-ashy.vercel.app/',
   },
+  {
+    name: 'Trivia',
+    img: 'https://i.ibb.co/x3Qmh1w/Captura-de-tela-de-2023-04-07-17-10-15.png',
+    stack: 'Front-End',
+    desc: `Projeto desenvolvido em grupo durante o módulo de front-end da Trybe,
+    onde tinhamos o objetivo de desenvolver um jogo de perguntas e respostas(Quiz), sendo 
+    possível fazer login, configurar as regras do jogo e vizualizar o ranking ao final de tudo `,
+    link: 'https://project-trivia-dolsrj5js-erickargollo.vercel.app/game',
+  },
+  {
+    name: 'TFC',
+    img: 'https://i.ibb.co/FJt2pRX/Captura-de-tela-de-2023-04-07-17-39-28.png',
+    stack: 'Full-Stack',
+    desc: `Primeiro projeto full-stack com o objetivo desenvolver uma tabela de ranking de times 
+    onde criei uma API Rest para o front-end utilizando o banco de dados MySQL, express,
+    sequelize ORM, TypeScript e a arquitetura MSC, onde é possível fazer o CRUD completo.`,
+    link: 'https://github.com/rogermarques08/trybe-futebol-clube',
+  },
 ];
 
 export default projects;
